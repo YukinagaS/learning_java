62cfd068d8743cb984ed21d05b4cdbd23fbdfe23
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-12-06 22:26:49 +0900

    Solve 2025, Day 1, p1

--- src/year2025/dayone.java
+++ src/year2025/dayone.java
@@ -4,16 +4,37 @@ import utilities.FileUtils;
 import java.util.ArrayList;
 
 public class dayone {
-  public static void main(String[] args) {
-      int dialPosition = 50;
-      String filePath = "data/2025_01.txt";
-      ArrayList<String> testData = FileUtils.readToArrayList(filePath);
 
-      for(String item : testData) {
-        char direction = item.charAt(0);
-        int distance = Integer.parseInt(item.substring(1));
-        String message = String.format("Direction: %s, Distance: %s", direction, distance);
-        System.out.println(message);
+  static void turnDial(ArrayList<String> data) {
+    int dialPosition = 50;
+    int count = 0;
+
+    for(String item : data) {
+      char direction = item.charAt(0);
+      int distance = Integer.parseInt(item.substring(1));
+      String message = String.format("Direction: %s, Distance: %s", direction, distance);
+      System.out.println(message);
+
+      if (direction == 'L') {
+        int newPosition = (dialPosition - distance + 100) % 100;
+        System.out.println("Move from " + dialPosition + " to " + newPosition);
+        if (newPosition == 0) count += 1;
+        dialPosition = newPosition;
+      } else {
+        int newPosition = (dialPosition + distance + 100) % 100;
+        System.out.println("Move from " + dialPosition + " to " + newPosition);
+        if (newPosition == 0) count += 1;
+        dialPosition = newPosition;
       }
+    }
+    System.out.println("Final count: " + count);
+
+  }
+  public static void main(String[] args) {
+      ArrayList<String> testData = FileUtils.readToArrayList("data/2025_01.txt");
+      ArrayList<String> puzzleData = FileUtils.readToArrayList("puzzle/2025_01.txt");
+
+      turnDial(testData);
+      turnDial(puzzleData);
   }
 }
