1652ac6a54dc9ede9f46b1618af9e5355546b14f
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-12-07 19:49:53 +0900

    途中 on 2025-D2P2

--- src/year2025/daytwo.java
+++ src/year2025/daytwo.java
@@ -3,6 +3,8 @@ package year2025;
 import utilities.FileUtils;
 import java.util.stream.LongStream;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class daytwo {
 
@@ -16,7 +18,7 @@ public class daytwo {
       result.add(LongStream.rangeClosed(rangeStart, rangeEnd));
     }
     return result;
-  }
+  } // End processData
 
   static long sumInvalids(ArrayList<LongStream> data) {
     ArrayList<Long> invalidIDs = new ArrayList<Long>();
@@ -36,19 +38,46 @@ public class daytwo {
       sum += item;
     }
     return sum;
-  }
+  } // end sumInvalids
+
+  static long checkP2(ArrayList<LongStream> data) {
+    ArrayList<Long> invalidIDs = new ArrayList<Long>();
+    Long sum = 0L;
+
+    for (LongStream idRange : data) {
+      idRange.forEach(n -> {
+        String strN = Long.toString(n);
+        long longLength = strN.length();
+
+        for (int i = 1; i <= strN.length() / 2; i++) {
+          String target = strN.substring(0, i);
+          Pattern pattern = Pattern.compile(target);
+          Matcher matcher = pattern.matcher(strN);
+          long matches = matcher.results().count();
+          System.out.println(strN + ": " + target);
+          System.out.println(matches == longLength / (i + 1));
+          if (matches == longLength / i ) invalidIDs.add(n);
+        }
+      });
+    }
+
+    for (Long item : invalidIDs) {
+      sum += item;
+    }
+    return sum;
+  } // end checkP2
 
   public static void main(String[] args) {
     String testData = FileUtils.readToString("data/2025_02.txt");
     ArrayList<LongStream> testProcessed = processData(testData);
-    long sumTest = sumInvalids(testProcessed);
+
+    // System.out.println("Test Invalid IDs: " + sumInvalids(testProcessed));
+    System.out.println("P2 test result: " + checkP2(testProcessed));
 
     // String puzzleData = FileUtils.readToString("puzzle/2025_02.txt");
     // ArrayList<LongStream> puzzleProcessed = processData(puzzleData);
     // long sumPuzzle = sumInvalids(puzzleProcessed);
 
-
-    System.out.println("Test Invalid IDs: " + sumTest);
     // System.out.println("Puzzle Invalid IDs: " + sumPuzzle);
-  } // End of main
+  } // End main
 }
