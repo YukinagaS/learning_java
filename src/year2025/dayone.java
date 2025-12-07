db928d50295e2ea0a97cb5921f42e8ef4bf5ffaa
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-12-07 15:39:50 +0900

    Working on D1P2

--- src/year2025/dayone.java
+++ src/year2025/dayone.java
@@ -8,33 +8,42 @@ public class dayone {
   static void turnDial(ArrayList<String> data) {
     int dialPosition = 50;
     int count = 0;
+    int zeroCount = 0; //P2
 
     for(String item : data) {
       char direction = item.charAt(0);
-      int distance = Integer.parseInt(item.substring(1));
-      String message = String.format("Direction: %s, Distance: %s", direction, distance);
+      int fullSpin = Integer.parseInt(item.substring(1));
+      int distance = fullSpin % 100;
+      int extraRevs = fullSpin / 100 % 100;
+
+      String message = String.format("Direction: %s, Distance: %s, Revs: %s", direction, distance, extraRevs);
       System.out.println(message);
+
+      zeroCount += extraRevs; // P2
 
       if (direction == 'L') {
         int newPosition = (dialPosition - distance + 100) % 100;
         System.out.println("Move from " + dialPosition + " to " + newPosition);
         if (newPosition == 0) count += 1;
+        if (distance >= dialPosition) zeroCount += 1; //P2
         dialPosition = newPosition;
       } else {
         int newPosition = (dialPosition + distance + 100) % 100;
         System.out.println("Move from " + dialPosition + " to " + newPosition);
         if (newPosition == 0) count += 1;
+        if (distance + dialPosition >= 100) zeroCount += 1; // P2
         dialPosition = newPosition;
       }
     }
-    System.out.println("Final count: " + count);
-
+    System.out.println("Final P1 count: " + count);
+    System.out.println("Final P2 count: " + zeroCount); // P2
   }
   public static void main(String[] args) {
       ArrayList<String> testData = FileUtils.readToArrayList("data/2025_01.txt");
       ArrayList<String> puzzleData = FileUtils.readToArrayList("puzzle/2025_01.txt");
 
-      turnDial(testData);
+      // turnDial(testData);
       turnDial(puzzleData);
+
   }
 }
