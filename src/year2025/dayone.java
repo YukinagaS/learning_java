31afc814429c26507d93f9c970445fab510f01ed
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-12-07 15:56:18 +0900

    Solve 2025-D1P2

--- src/year2025/dayone.java
+++ src/year2025/dayone.java
@@ -14,9 +14,9 @@ public class dayone {
       char direction = item.charAt(0);
       int fullSpin = Integer.parseInt(item.substring(1));
       int distance = fullSpin % 100;
-      int extraRevs = fullSpin / 100 % 100;
+      int extraRevs = fullSpin / 100;
 
-      String message = String.format("Direction: %s, Distance: %s, Revs: %s", direction, distance, extraRevs);
+      String message = String.format(item + "=> Direction: %s, Distance: %s, Revs: %s", direction, distance, extraRevs);
       System.out.println(message);
 
       zeroCount += extraRevs; // P2
@@ -25,7 +25,7 @@ public class dayone {
         int newPosition = (dialPosition - distance + 100) % 100;
         System.out.println("Move from " + dialPosition + " to " + newPosition);
         if (newPosition == 0) count += 1;
-        if (distance >= dialPosition) zeroCount += 1; //P2
+        if (dialPosition != 0 && distance >= dialPosition) zeroCount += 1; //P2
         dialPosition = newPosition;
       } else {
         int newPosition = (dialPosition + distance + 100) % 100;
@@ -34,6 +34,7 @@ public class dayone {
         if (distance + dialPosition >= 100) zeroCount += 1; // P2
         dialPosition = newPosition;
       }
+      System.out.println("Zero count: " + zeroCount);
     }
     System.out.println("Final P1 count: " + count);
     System.out.println("Final P2 count: " + zeroCount); // P2
